e495b342bdfadf1d429e6b6321bb0c8dd991e8f8
Kafabi Aulia Fasyah <150753433+Kafabi0@users.noreply.github.com>
2025-10-24 15:59:19 +0700

    perbaikan lagi

--- indosemecb2b/lib/screen/checkout_screen.dart
+++ indosemecb2b/lib/screen/checkout_screen.dart
@@ -44,87 +44,115 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
         titleTextStyle: const TextStyle(color: Colors.black, fontSize: 18),
       ),
 
-      body: _isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : Padding(
-              padding: const EdgeInsets.all(16),
-              child: SingleChildScrollView(
-                padding: const EdgeInsets.only(bottom: 150),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      "Alamat Pengiriman",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                    ),
-                    const SizedBox(height: 8),
-                    Container(
-                      padding: const EdgeInsets.all(16),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        border: Border.all(color: Colors.grey.shade300),
-                      ),
-                      child: Row(
-                        children: [
-                          Icon(Icons.location_on,
-                              color: Colors.blue[700], size: 28),
-                          const SizedBox(width: 12),
-                          Expanded(
-                            child: Text(
-                              widget.alamat?['alamat_lengkap'] ??
-                                  'Alamat belum lengkap',
-                              style: const TextStyle(fontSize: 14),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    const Text("Daftar Produk",
+      body:
+          _isLoading
+              ? const Center(child: CircularProgressIndicator())
+              : Padding(
+                padding: const EdgeInsets.all(16),
+                child: SingleChildScrollView(
+                  padding: const EdgeInsets.only(bottom: 150),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Alamat Pengiriman",
                         style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold)),
-                    const SizedBox(height: 10),
-
-                    ..._cartItems.map((item) {
-                      return Container(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 12),
-                        margin: const EdgeInsets.only(bottom: 10),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Container(
+                        padding: const EdgeInsets.all(16),
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(12),
                           border: Border.all(color: Colors.grey.shade300),
                         ),
                         child: Row(
                           children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(8),
-                              child: Image.network(
-                                item.imageUrl ?? "",
-                                width: 65,
-                                height: 65,
-                                fit: BoxFit.cover,
-                              ),
+                            Icon(
+                              Icons.location_on,
+                              color: Colors.blue[700],
+                              size: 28,
                             ),
                             const SizedBox(width: 12),
                             Expanded(
                               child: Text(
-                                item.name,
-                                maxLines: 2,
-                                overflow: TextOverflow.ellipsis,
+                                widget.alamat?['alamat_lengkap'] ??
+                                    'Alamat belum lengkap',
+                                style: const TextStyle(fontSize: 14),
                               ),
                             ),
-                            Text("Rp${item.totalPrice.toStringAsFixed(0)}",
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.bold)),
                           ],
                         ),
-                      );
-                    }),
-                  ],
+                      ),
+                      const SizedBox(height: 20),
+                      const Text(
+                        "Daftar Produk",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+
+                      ..._cartItems.map((item) {
+                        return Container(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 12,
+                          ),
+                          margin: const EdgeInsets.only(bottom: 10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            border: Border.all(color: Colors.grey.shade300),
+                          ),
+                          child: Row(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(8),
+                                child: Image.network(
+                                  item.imageUrl ?? "",
+                                  width: 65,
+                                  height: 65,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              const SizedBox(width: 12),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      item.name,
+                                      maxLines: 2,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                    const SizedBox(height: 4),
+                                    Text(
+                                      "Jumlah: ${item.quantity}",
+                                      style: TextStyle(
+                                        color: Colors.grey[600],
+                                        fontSize: 13,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Text(
+                                "Rp${item.totalPrice.toStringAsFixed(0)}",
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      }),
+                    ],
+                  ),
                 ),
               ),
-            ),
 
       bottomNavigationBar: SafeArea(
         child: Container(
@@ -133,7 +161,10 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
             color: Colors.white,
             boxShadow: [
               BoxShadow(
-                  color: Colors.black12, blurRadius: 8, offset: Offset(0, -2))
+                color: Colors.black12,
+                blurRadius: 8,
+                offset: Offset(0, -2),
+              ),
             ],
           ),
           child: Column(
@@ -142,13 +173,16 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const Text("Total Pembayaran",
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+                  const Text(
+                    "Total Pembayaran",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                  ),
                   Text(
                     "Rp${getTotal().toStringAsFixed(0)}",
                     style: const TextStyle(
-                        fontSize: 18, fontWeight: FontWeight.bold),
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ],
               ),
@@ -159,10 +193,13 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
                   backgroundColor: Colors.blue[700],
                   minimumSize: Size(double.infinity, 48),
                   shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12)),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
                 ),
-                child: const Text("Lanjut ke Pembayaran",
-                    style: TextStyle(color: Colors.white)),
+                child: const Text(
+                  "Lanjut ke Pembayaran",
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
             ],
           ),
